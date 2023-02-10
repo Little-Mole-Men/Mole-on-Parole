5b2ae4ddbf373665c76ccbbcb3a0d8c868ce4ed1
srw73 <sam.r.williams13@gmail.com>
2023-02-10 22:34:00 +0000

    interfaces n such

---
+++ IDrawable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mole_on_Parole
+{
+    public interface IDrawable
+    {
+        public void Draw();
+    }
+}
